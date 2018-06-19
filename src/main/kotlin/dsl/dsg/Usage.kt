186bdd7d29ca27a9faf51d4a631041a255f7a406
Joao Antonio Ferreira <joao.parana@gmail.com>
2018-06-18 23:15:15 -0300

    minor update

--- src/main/kotlin/dsl/dsg/Usage.kt
+++ src/main/kotlin/dsl/dsg/Usage.kt
@@ -6,7 +6,7 @@ fun main(args: Array<String>) {
     val temperatura = 301
     val UAM02_PXI_2_T08_C_All = 1248
 
-    val dsg = dataSourceGroup firstIndex 5 queue UAM02_PXI_2_T08_C_All dsType temperatura item 319 containing dataSources {
+    val dsg = dataSourceGroup setup "\${enable(IT.debug)}" firstIndex 5 queue UAM02_PXI_2_T08_C_All dsType temperatura item 319 containing dataSources {
         dataSource name "xx" title "title xx " desc "desc xx" with config {
             piTagConfig conf "{tagName:'STH     .MEDICAO NIVEL JUSANTE HJ2 (U18) LM-04           '}"
         }
@@ -14,7 +14,7 @@ fun main(args: Array<String>) {
             piTagConfig conf "{tagName:'STH     .MEDICION NIVEL AGUAS ABAJO HJ1 (U7) LM-03       '}"
         }
         dataSource name "zz" title "title zz " desc "desc \${IT}.name"
-    } setup "\${enable(IT.debug)}" terms listOf(4309,4310,4312) info "{'item-RTD':'1-\${21+IT.index}', 'item':'1', 'RTD':'\${21+IT.index}', 'Planilha':'Estator'}" using rules {
+    }  terms listOf(4309,4310,4312) info "{'item-RTD':'1-\${21+IT.index}', 'item':'1', 'RTD':'\${21+IT.index}', 'Planilha':'Estator'}" using rules {
         rule code "x = 1" title "x vale 1 "
         rule code "debug = true" title "habilitando o DEBUG"
     }
